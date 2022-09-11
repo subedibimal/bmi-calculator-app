48b1512ba41271f6a9d75849631178e28f1418c0
subedibimal <bimalsubedi04@gmail.com>
2022-09-11 15:49:34 +0545

    fix: minor fixes

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'screens/input_page.dart';
-import 'constants.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
